011188fb3579c221a9c701d9d2e0a52b03b3f201
rischuhm <richard.schuhmann94@gmail.com>
2021-04-15 18:59:16 +0200

    added requirements

--- conf/gunicorn_conf.py
+++ conf/gunicorn_conf.py
@@ -1,4 +1,4 @@
-command = '/home/ubuntu/mapps/mappenv/bin/gunicorn'
+command = '/home/ubuntu/mapps/mappsenv/bin/gunicorn'
 pythonpath = '/home/ubuntu/mapps/mapps'
 bind = '192.168.178.36:8123'
 workers = 3
