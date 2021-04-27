30060a323a09165354e440ceadbaf5c084e16594
rischuhm <richard.schuhmann94@gmail.com>
2021-04-27 09:22:52 +0200

    changed docker start.sh for better automation performance

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-
 certbot --nginx --agree-toos --email richard.schuhmann94@gmail.com -d mapps-leipzig.tk -d www.mapps-leipzig.tk
 ./manage.py migrate --noinput
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
 ./manage.py collectstatic --noinput
 
 supervisord -c /code/app/docker/supervisord.conf
