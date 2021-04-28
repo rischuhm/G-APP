6e40eff0219d98262134cddbd06278166d96fd75
rischuhm <richard.schuhmann94@gmail.com>
2021-04-28 12:12:05 +0200

    changed order of commands in start.sh for better work

--- docker/start.sh
+++ docker/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-
-certbot --nginx --agree-tos --email richard.schuhmann94@gmail.com -d mapps-leipzig.tk -d www.mapps-leipzig.tk
 ./manage.py migrate --noinput
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'pass')" | python manage.py shell
 ./manage.py collectstatic --noinput
 
 supervisord -c /code/app/docker/supervisord.conf
+
+certbot --nginx --agree-tos --email richard.schuhmann94@gmail.com -d mapps-leipzig.tk -d www.mapps-leipzig.tk
